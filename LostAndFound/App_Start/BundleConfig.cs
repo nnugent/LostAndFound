f8b70a8f9c17e61563e2b2c58b1842069eb86973
Nathen R Nugent <nrnugent96@gmail.com>
2018-06-21 08:27:29 -0500

    Add styling from bootswatch.com

--- LostAndFound/App_Start/BundleConfig.cs
+++ LostAndFound/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace LostAndFound
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.slate.css",
                       "~/Content/site.css"));
         }
     }
